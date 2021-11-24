5b0326b062534ffaa4e42381975f25cbc175e548
Ayose18 <Ayosetrujillo18@gmail.com>
2021-11-24 20:48:40 +0000

    first commit

---
+++ src/components/ElementTable.vue
@@ -0,0 +1,45 @@
+<template>
+<div class= "element">{{Elementname}},{{Atomicnumber}},{{ElementSymbol}},{{atomicMass}}</div>
+</template>
+
+<script>
+export default {
+
+  name: "ElementTable",
+  props: {
+
+    Elementname: {
+      type: String,
+      default: "Hydrogen",
+    },
+
+    Atomicnumber: {
+      type: Number,
+      default: 1,
+    },
+    ElementSymbol: {
+      type: String,
+      default: "H",
+    },
+    atomicMass: {
+      type: String,
+      default: "1.00794(4)",
+    },
+    cpkHexColor: {
+      type: String,
+      default: "FFFFFF",
+    },
+  },
+
+}; </script>
+
+<style scoped>
+
+.element{
+border: 1px var(--cpkHexColor) solid;
+width:80px;
+height: 80px;
+font-size: 12px;
+
+}
+</style>
---
+++ src/components/PeriodicTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ElementTable style= "background-color : red" :Atomicnumber = "e.atomicNumber" :atomicMass = "e.atomicMass" :ElementSymbol = "e.symbol" :Elementname = "e.name" v-for="(e, index) in elements" :key = "index"></ElementTable>
+  </div>
+</template>
+
+<script>
+import ElementTable from "./ElementTable.vue";
+export default {
+
+  name: "PeriodicTable",
+  mounted() {
+    fetch("data/data.json")
+      .then(response => response.json())
+
+      .then(data => { this.elements = data; });
+  },
+
+  data() {
+    return {
+      elements: []
+    };
+  },
+
+  components: {
+    ElementTable
+
+  }
+
+}; </script>
+
+<style scoped></style>
